add794575bd887df198913bf929b03358d165c52
valentina-26 <160797346+valentina-26@users.noreply.github.com>
2024-06-21 06:18:02 -0500

    feat: :sparkles: Busqueda por descripcion funcional

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@ addEventListener("DOMContentLoaded", async () => {
 
     input__search.addEventListener("input", async e => {
         
-        const searchTerm = e.target.value.toLowerCase().trim(); // Obtener el valor del input y convertirlo a minusculas y sin espacios
+        const searchTerm = e.target.value.toLowerCase().trim().normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Obtener el valor del input y convertirlo a minusculas y sin espacios
 
         const filteredProfiles = info.filter(person => {
-            return person.name_full.toLowerCase().includes(searchTerm); // Comparar si el nombre de la persona incluye el termino de busqueda
+            return person.name_full.toLowerCase().includes(searchTerm) || person.description.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().includes(searchTerm)  ; // Comparar si el nombre de la persona incluye el termino de busqueda
         });
     
         main__section__persons.innerHTML = await profiles(filteredProfiles);
